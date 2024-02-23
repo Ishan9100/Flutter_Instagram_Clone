15bbebac44345ed9312c8067a2a0f6d75d0ad058
Ishan Prabhune <ishan.9100@gmail.com>
2024-02-22 22:18:34 -0600

    Initial commit

---
+++ flutter_application_instagramclone/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Flutter\\Projects\\flutter_application_instagramclone" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=E:\\Flutter\\Projects\\flutter_application_instagramclone"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Flutter\\Projects\\flutter_application_instagramclone\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Flutter\\Projects\\flutter_application_instagramclone"
+  "FLUTTER_TARGET=E:\\Flutter\\Projects\\flutter_application_instagramclone\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Flutter\\Projects\\flutter_application_instagramclone\\.dart_tool\\package_config.json"
+)
